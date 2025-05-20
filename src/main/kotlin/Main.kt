22fee50a0e8fa4f0e3c08b280e789415f6444999
krishnanIT <bk111100001100@gmail.com>
2025-05-20 23:54:49 +0530

    kotlin folder create code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,13 @@
-"""fun main()
+/*fun main()
 {
     println("Kotlin developer")
 }
+*/
 // kotlin file convert to the jar file
 //kotlinc kotlinjar.kt -include-runtime -d kt_jar.jar
 // jar file run command
 //java -jar kt_jar.jar
-"""
+
 
 // Kotlin folder creat code
 import java.io.File

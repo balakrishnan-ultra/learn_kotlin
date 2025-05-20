92f66a0c3fff39b8bc224906025edb46115f428b
krishnanIT <bk111100001100@gmail.com>
2025-05-20 23:53:44 +0530

    kotlin folder create code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-fun main()
+"""fun main()
 {
     println("Kotlin developer")
 }
@@ -6,3 +6,26 @@ fun main()
 //kotlinc kotlinjar.kt -include-runtime -d kt_jar.jar
 // jar file run command
 //java -jar kt_jar.jar
+"""
+
+// Kotlin folder creat code
+import java.io.File
+
+fun createFolder(path: String) {
+    val folder = File(path)
+    if (!folder.exists()) {
+        folder.mkdirs()
+        if (folder.exists()) {
+            println("Folder created successfully at $path")
+        } else {
+            println("Failed to create folder at $path")
+        }
+    } else {
+        println("Folder already exists at $path")
+    }
+}
+
+fun main() {
+    val folderPath = "path/to/your/new/folder"
+    createFolder(folderPath)
+}
